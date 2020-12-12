d37fa99e9389359d028d25e2e5ae5bab0abf2915
Brinna Francesca Gorena Llado <a2019119323@estudiantes.upsa.edu.bo>
2020-12-12 11:56:06 -0400

    Clase Numero creada.

---
+++ Operaciones/Operaciones/Models/Numero.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Operaciones.Models
+{
+    public class Numero
+    {
+        [Key]
+        public int numero { get; set; }
+        public string mensaje { get; set; }
+    }
+}
